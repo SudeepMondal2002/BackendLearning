75e1adef1b31ebd649bbf8605cc05a118938f123
SudeepMondal2002 <mondalsudeep13@gmail.com>
2024-09-23 23:36:14 +0530

    commit

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
-  try {g
+  try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     console.log(`\n MONGODB connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
